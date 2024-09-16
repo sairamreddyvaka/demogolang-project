7caedb1cacb7a90d604160d18ebcfd0c20a2b9a2
sairamreddyvaka <sairamreddy332@gmail.com>
2024-09-16 15:22:16 +0100

    made changes

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -3,3 +3,4 @@ set -e #exits any errors
 
 docker run -d --name jenkins -p 8081:8080 -p 50000:50000 -v jenkins_data:/var/jenkins_home jenkins/jenkins:lts
 docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+docker exec jenkins /bin/bash -c "echo 'jenkins' | /usr/local/bin
